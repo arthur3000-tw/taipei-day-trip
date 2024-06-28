d87f141da4f3d2bb1d7ef6497cf8871a47d14ceb
arthur3000 <arthur3000.tw@gmail.com>
2024-06-28 08:51:30 +0800

    Modify delete /api/booking, it deletes all user bookings

--- app.py
+++ app.py
@@ -534,10 +534,10 @@ def deleteBooking(userInfo:UserInfo):
     # 取出資料
     userId = userInfo.data.id
     # 資料庫指令
-    sql = "DELETE FROM booking WHERE user_id = %s ORDER BY log_time DESC LIMIT 1"
+    sql = "DELETE FROM booking WHERE user_id = %s"
     val = (userId,)
     result = insertDB(sql,val)
-    if result == 1:
+    if result != 0:
         return OK(ok=True)
     else:
         return JSONResponse(status_code=400, content=Error(error=True, message="沒有可以刪除的資料").model_dump())
