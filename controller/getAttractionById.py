de8f3a6d02f644209ba0460fd3403b0193e39a3b
arthur3000 <arthur3000.tw@gmail.com>
2024-07-09 19:50:38 +0800

    Modify controller/getAttractionById.py

---
+++ controller/getAttractionById.py
@@ -0,0 +1,15 @@
+from fastapi import APIRouter, Request
+from fastapi.responses import JSONResponse
+from model.AttractionModel import AttractionID
+from model.responseModel import Error
+
+router = APIRouter()
+
+# 根據 id 取得 attraction 資料
+@router.get(path="/api/attraction/{attractionId}", responses={400: {"model": Error}})
+async def get_api_attraction_id(request: Request, attractionId: int) -> AttractionID:
+    try:
+        result = get_attraction_id(attractionId)
+        return result
+    except ValueError:
+        return JSONResponse(status_code=400, content=Error(error=True, message="未找到此 ID 之景點資料").model_dump())
