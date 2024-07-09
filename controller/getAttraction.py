985d80a011d03ff428b9d6ffad6d21679cacaf28
arthur3000 <arthur3000.tw@gmail.com>
2024-07-09 15:37:21 +0800

    Add AttractionModel.py

--- controller/getAttraction.py
+++ controller/getAttraction.py
@@ -1,4 +1,13 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Request, Query
+
 
 router = APIRouter()
 
+# 取得 attractions 資料列表
+@router.get(path="/api/attractions")
+async def get_api_attractions(request: Request, page: int = Query(default=0, ge=0), keyword: str = None) -> Attractions:
+    try:
+        result = get_attractions(page, keyword)
+        return result
+    except ValueError:
+        return JSONResponse(status_code=400, content=Error(error=True, message="沒有符合的資料").model_dump())
