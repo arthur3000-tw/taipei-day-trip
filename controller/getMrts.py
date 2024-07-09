42830f62512a051ca9d1b9ce432b729d5b253bd8
arthur3000 <arthur3000.tw@gmail.com>
2024-07-09 20:38:38 +0800

    Modify contorller/getMrts.py

--- controller/getMrts.py
+++ controller/getMrts.py
@@ -6,5 +6,6 @@ router = APIRouter()
 # 取得所有捷運站名稱列表，按照週邊景點的數量由大到小排序
 @router.get(path="/api/mrts")
 async def get_api_mrts(request: Request) -> DataList:
-    result = get_mrts()
+    myDB = request.app.state.db
+    result = get_mrts(myDB)
     return result
