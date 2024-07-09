01696538cbb702728b0483a63a5fab6f1ad47d24
arthur3000 <arthur3000.tw@gmail.com>
2024-07-09 20:34:32 +0800

    Modify model/getMrts.py

--- app.py
+++ app.py
@@ -203,18 +203,7 @@ def insertDB(sql, val):
 
 
 
-# 捷運站按照景點數量排序
-# 輸出
-# DataList       資料
-def get_mrts():
-    sql = "SELECT mrt.name, count(attraction_mrt.mrt_id) as times \
-           FROM attraction_mrt INNER JOIN mrt ON mrt.id = attraction_mrt.mrt_id \
-           GROUP BY attraction_mrt.mrt_id ORDER BY times DESC"
-    results = queryDB(sql)
-    result = []
-    for eachDict in results:
-        result.append(eachDict["name"])
-    return DataList(data=result)
+
 
 
 # 註冊帳號
