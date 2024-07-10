ec31bec5487dc5c4076a91622e61111c41c3e777
arthur3000 <arthur3000.tw@gmail.com>
2024-07-10 16:50:41 +0800

    Modify controller/httpExceptionHandler.py

--- controller/httpExceptionHandler.py
+++ controller/httpExceptionHandler.py
@@ -1,6 +1,6 @@
 from fastapi import Request, HTTPException, status
 from fastapi.responses import JSONResponse
-from model import responseModel
+from model.ResponseModel import Error
 
 
 class CustomHttpException(HTTPException):
@@ -9,4 +9,4 @@ class CustomHttpException(HTTPException):
 # http exception handling
 async def custom_http_exception_handler(request: Request, exc: HTTPException):
     if exc.status_code == status.HTTP_403_FORBIDDEN:
-        return JSONResponse(status_code=403, content=responseModel.Error(error=True, message="尚未登入").model_dump())
+        return JSONResponse(status_code=403, content=Error(error=True, message="尚未登入").model_dump())
