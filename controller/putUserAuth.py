631b8ea07c03cddf06ca10e653f097185bb397d6
arthur3000 <arthur3000.tw@gmail.com>
2024-07-10 12:45:06 +0800

    Modify controller/putUserAuth.py

--- controller/putUserAuth.py
+++ controller/putUserAuth.py
@@ -9,8 +9,10 @@ router = APIRouter()
 # 登入會員帳戶
 @router.put(path="/api/user/auth", responses={400: {"model": Error}})
 async def put_api_user_auth(request: Request, userAuth: UserAuth) -> JWT:
+    my_jwt = router.app.state.jwt
+    myDB = router.app.state.db
     try:
-        result = validateUser(userAuth)
+        result = validateUser(myDB, my_jwt, userAuth)
         return result
     except ValueError:
         return JSONResponse(status_code=400, content=Error(error=True, message="未找到此 email 或密碼錯誤").model_dump())
