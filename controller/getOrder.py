f31237402f366a83000d9018ffb6ddc510af951e
arthur3000 <arthur3000.tw@gmail.com>
2024-07-10 16:24:50 +0800

    Modify controller/getOrder.py

---
+++ controller/getOrder.py
@@ -0,0 +1,24 @@
+from fastapi import APIRouter, Request, Depends
+from fastapi.security import HTTPAuthorizationCredentials, HTTPBearer
+from fastapi.responses import JSONResponse
+from typing import Annotated
+from model.isLogin import isLogin
+from model.ResponseModel import Error
+
+router = APIRouter()
+
+security = HTTPBearer()
+
+# 根據訂單編號取得訂單資訊
+@router.get(path="/api/order/{orderNumber}")
+async def get_api_order(request: Request, credentials: Annotated[HTTPAuthorizationCredentials, Depends(security)], orderNumber: str):
+    my_jwt = request.app.state.jwt
+    try:
+        userInfo = my_jwt.validate(credentials.credentials)
+        if isLogin(userInfo):
+            result = getOrder(userInfo,orderNumber)
+            return result
+        else:
+            return JSONResponse(status_code=403, content=Error(error=True, message="尚未登入").model_dump())
+    except:
+        return JSONResponse(status_code=500, content=Error(error=True, message="伺服器內部錯誤").model_dump())
