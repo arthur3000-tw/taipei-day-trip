79c056cb8d770784db9ec9e2d5db85a8ac8790d4
arthur3000 <arthur3000.tw@gmail.com>
2024-07-10 11:12:02 +0800

    Modify controller/postUser.py

--- controller/postUser.py
+++ controller/postUser.py
@@ -1,13 +1,15 @@
 from fastapi import APIRouter, Request
 from fastapi.responses import JSONResponse
 from model.responseModel import Error
+from model.signUp import SignUpInfo, signUp
 
 router = APIRouter()
 
 # 註冊會員帳戶
 @router.post(path="/api/user", responses={400: {"model": Error}})
 async def post_api_user(request: Request, signUpInfo: SignUpInfo) -> OK:
+    myDB = router.app.state.db
     try:
-        return signUp(signUpInfo)
+        return signUp(myDB, signUpInfo)
     except ValueError:
         return JSONResponse(status_code=400, content=Error(error=True, message="重複的 email").model_dump())
