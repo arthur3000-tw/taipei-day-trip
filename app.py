476982c8136f8f1bdabc78d160fc8b4be4d2b4b6
arthur3000 <arthur3000.tw@gmail.com>
2024-07-10 12:42:06 +0800

    Modify model/validateUser.py

--- app.py
+++ app.py
@@ -95,28 +95,6 @@ class Status(BaseModel):
     data: int | None = None
 
 
-# 驗證使用者
-# 輸入
-# userAuth 格式資料
-# 輸出
-# 密碼正確： JWT 格式資料
-# 密碼錯誤： ValueError()
-def validateUser(userAuth: UserAuth):
-    # 資料庫查詢是否有此 email
-    sql = "SELECT * FROM user WHERE email = %s"
-    val = (userAuth.email,)
-    result = queryDB(sql, val)
-    # 有找到此 email 且密碼正確
-    if len(result) == 1 and validatePassword(userAuth.password, result[0]["password"]):
-        # 進行製作 JWT，設置七天時間期限
-        payload = {"id": result[0]["id"], "name": result[0]
-                   ["name"], "email": result[0]["email"]}
-        return JWT(token=generateJWT(payload, EXPIRED_DAYS))
-    # 未找到此 email 或密碼錯誤
-    else:
-        raise ValueError()
-
-
 # 實體化 MyJWT
 my_jwt = MyJWT.MyJWT(jwt_secret_key = os.environ.get("JWT_SECRET_KEY"), expired_days = 7, jwt_algorithm = "HS256")
 # MyJWT instance 存放於 app.state 中
