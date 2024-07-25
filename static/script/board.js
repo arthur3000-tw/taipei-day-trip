6aa47a3eb979dcb8f026541d3e1149743f6c9c1e
arthur3000 <arthur3000.tw@gmail.com>
2024-07-25 16:42:24 +0800

    Modify /static/board.html /static/script/board.js

---
+++ static/script/board.js
@@ -0,0 +1,33 @@
+let upload_button = document.querySelector("#upload-button");
+
+upload_button.addEventListener("click", upload);
+
+function upload() {
+  const board_content = document.querySelector("#board-content");
+  const board_file = document.querySelector("#board-file");
+  const form_data = new FormData();
+  const file = board_file.files[0];
+  console.log(file);
+
+  form_data.append("file", file);
+  form_data.append("content", board_content.value)
+
+  const url = "/api/board";
+  const method = "POST";
+  const body = form_data;
+
+  fetch(url, {
+    method: method,
+    body: JSON.stringify(body),
+  })
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      if (data["error"] === true) {
+        console.log("錯誤")
+      } else if (data["ok"] === true) {
+        console.log("上傳成功")
+      }
+    });
+}
